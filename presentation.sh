97581364109e58bcd3af8460a0fa21d1d4b5f5bc
d1hn1 <daniellopezsantiago27@gmail.com>
2025-01-18 16:52:10 +0100

    Updated!

--- presentation.sh
+++ presentation.sh
@@ -2,6 +2,7 @@
 
 WIDTH=$(stty size | cut -d " " -f 2)
 HEIGHT=$(stty size | cut -d " " -f 1)
+TRANS_TIME=100
 
 ESC_END="\e[0m"
 CLR_RED="\e[31m"
@@ -33,4 +34,103 @@ else
 	fi
 fi
 
+# CLEAR
+clear
 
+wrap_text () {
+
+	movec $2 $1
+
+	LENGTH=$3
+	TEXT=$4
+	COUNT=0
+
+	for (( x=0; x<${#TEXT}; x++ )); do
+		if [[ $LENGTH == $COUNT ]]; then
+			moved 1
+			movel $LENGTH
+			echo -n -e ${TEXT:$x:1}
+			COUNT=0
+		else
+			echo -n -e ${TEXT:$x:1}
+		fi
+
+		((COUNT++))
+	done
+}
+
+# wrap-text(By)(Word)
+# wrap_textbw X Y LENGTH TEXT
+wrap_textbw () {
+
+	movec $2 $1
+
+	LENGTH=$3
+	TEXT=$4
+	COUNT=0
+	COUNT_BCK=0
+
+	for (( x=0; x<${#TEXT}; x++ )); do
+
+		if [[ ${TEXT:$x:1} == " " ]]; then
+			echo -n " " 
+			((COUNT++))
+		fi
+
+		((COUNT_BCK++))
+
+		if [[ $LENGTH == $COUNT ]]; then
+			moved 1
+			movel $COUNT_BCK
+			echo -n -e ${TEXT:$x:1}
+			COUNT=0
+			COUNT_BCK=0
+		else
+			echo -n -e ${TEXT:$x:1}
+		fi
+
+	done
+}
+
+echo_text () {
+	movec $2 $1
+	echo -n -e $3
+}
+
+transition () {
+	movec 0 0
+	BT=0
+	for y in $( seq 1 $HEIGHT); do
+		for x in $( seq 1 $WIDTH); do
+			if [ $BT -eq 0 ]; then
+				echo -n -e "\033[48;5;255m \033[0m"
+				BT=1
+			else
+				echo -n " "
+				BT=0
+			fi
+			for (( x=0; x<$TRANS_TIME; x++)); do
+				continue
+			done
+		done
+	done
+
+	movec 0 0
+	for y in $( seq 1 $HEIGHT); do
+		for x in $( seq 1 $WIDTH); do
+			echo -n " "
+			for (( x=0; x<$TRANS_TIME; x++)); do
+				continue
+			done
+		done
+	done
+}
+
+first_slide () {
+	FIRST_TEXT="Daniel Lopez"
+	SECOND_TEXT="|||||||||||||||"
+	THIRD_TEXT="Presentacion ASO SYSSTAT"
+	wrap_textbw $((WIDTH / 2 - 10)) $((HEIGHT / 2 + 1)) 1 "$FIRST_TEXT"
+	wrap_text $((WIDTH / 2)) $(((HEIGHT / 2) + 1 - (${#SECOND_TEXT} / 2))) 1 "$SECOND_TEXT"
+	wrap_textbw $((( WIDTH / 2 ) + 5 )) $((HEIGHT / 2 - 1)) 2 "$THIRD_TEXT"
+}
